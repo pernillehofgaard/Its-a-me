9f16f6f13d57640fde761acb2bdb557efcf77c10
pernillehofgaard <pernille.hofgaard@hotmail.com>
2025-05-23 10:04:25 +0200

    finished hobbies session for now

--- src/Pages/Hobbies.jsx
+++ src/Pages/Hobbies.jsx
@@ -1,24 +1,21 @@
 import React from 'react';
 import Header from "../Components/Header";
 import Card from '../Components/Card';
-
-const hobbies = [
-  {title: "3D-printing", id:1 },
-  {title: "3D-modelling", id:2 },
-  {title: "workingout", id:3 },
-  {title: "gaming", id:4 },
-];
+import hobbies from '../Info files/hobbiesInfo';
+import './Hobbies.css';
 
 export default function Hobbies(){
   return (
-    <div>
+    <div className="hobbies">
       <Header />
-      <div className="Hobbies">
-      <h1>Hobbies</h1>
-      <Card title={hobbies[0].title} description="asdfasdjfiwejfoweifjo"/>
+      <div className="hobbies-title">
+        <h1>Hobbies</h1>
       </div>
-      
-  </div>
+        {hobbies.map((info) =>(
+            <Card title={info.title} description={info.description}/>
+
+        ))} 
+    </div>
   );
       
 }
